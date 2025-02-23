392bfab4df2da3b8adf998ca4c83399ab250bbd2
Bianka R <bianka_romero@me.com>
2025-02-23 17:32:36 +0100

    clean code comments, remove console.log

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-console.log("running")
-
+// Variable selectors
 const getMenu = document.querySelector(".hamburger-icon")
 const navMenu = document.querySelector(".nav-links")
 
-
+// Hamburger menu
 getMenu.addEventListener("click", () => {
-  console.log("hamburger clicked")
   getMenu.classList.toggle("open")
   navMenu.classList.toggle("open")
 })
